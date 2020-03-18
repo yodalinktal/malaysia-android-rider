af3e8b1ad6b149a6c859e6c2c74bbee0f8be7314
yoda <yoda@linktal.com>
2020-03-18 13:50:54 +0800

    优化代码

--- app/src/main/java/com/bsmart/pos/rider/base/api/Api.java
+++ app/src/main/java/com/bsmart/pos/rider/base/api/Api.java
@@ -35,9 +35,9 @@ public class Api {
 
     public static String baseUrl() {
         if (BuildConfig.DEBUG) {
-            return "http://192.168.8.165:8080/pos/api/";
+            return "http://47.254.215.143/pos/api/";
         } else {
-            return "http://192.168.8.165:8080/pos/api/";
+            return "http://47.254.215.143/pos/api/";
         }
     }
 }
